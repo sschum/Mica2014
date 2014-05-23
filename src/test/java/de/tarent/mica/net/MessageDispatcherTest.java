3b9b4a7dee0818f952e49c090b57103078402601
sschum <s.schumann@tarent.de>
2014-05-23 22:47:25 +0200

    * Tests für den MessageDispatcher

---
+++ src/test/java/de/tarent/mica/net/MessageDispatcherTest.java
@@ -0,0 +1,34 @@
+package de.tarent.mica.net;
+
+import static org.mockito.Mockito.*;
+import static de.tarent.mica.net.MessageCode.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+public class MessageDispatcherTest {
+
+	WebSocketController controller;
+	MessageDispatcher toTest;
+	
+	@Before
+	public void setup(){
+		controller = Mockito.mock(WebSocketController.class);
+		toTest = spy(new MessageDispatcher(controller));
+	}
+	
+	private String msg(MessageCode code) {
+		return code.getCode() + ": Text";
+	}
+	
+	@Test
+	public void onMessage(){
+		toTest.onMessage(msg(HELLO));
+		verify(controller).rename();
+		
+		toTest.onMessage(msg(PLACE_SHIPS));
+		verify(controller).placeShips();
+	}
+
+}
