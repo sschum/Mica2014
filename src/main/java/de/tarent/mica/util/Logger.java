1a17009d799c5b08989d5c1829438585892e1434
sschum <s.schumann@tarent.de>
2014-05-23 21:47:21 +0200

    * Eigene Logging-Klasse (verwendet JUL)

---
+++ src/main/java/de/tarent/mica/util/Logger.java
@@ -0,0 +1,35 @@
+package de.tarent.mica.util;
+
+import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.LogManager;
+
+public class Logger {
+	private static java.util.logging.Logger LOG = java.util.logging.Logger.getLogger(de.tarent.mica.util.Logger.class.getName());
+	
+	static {
+		String loggingfile = System.getProperty("java.util.logging.config.file");
+		if (loggingfile == null || loggingfile.isEmpty()) {
+			URL url = null;
+			try {
+				url = Logger.class.getClassLoader().getResource("logging.properties");
+				if (url == null) {
+					System.err.println("Cannot find logging.properties.");
+				} else {
+					LogManager.getLogManager().readConfiguration(url.openStream());
+				}
+			} catch (Exception e) {
+				System.err.println("Error reading logging.properties from '" + url + "': " + e);
+			}
+		}
+	}
+	
+	public static void debug(String message){
+		debug(message, null);
+	}
+	
+	public static void debug(String message, Throwable t){
+		if(t == null) LOG.log(Level.FINER, message);
+		else LOG.log(Level.FINER, message, t);
+	}
+}
