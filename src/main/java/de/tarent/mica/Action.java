30f9c214df1d1d804447bc5a1a32af5fb0262a05
sschum <s.schumann@tarent.de>
2014-06-04 19:24:44 +0200

    * HashCode und Equals von Action überladen

--- src/main/java/de/tarent/mica/Action.java
+++ src/main/java/de/tarent/mica/Action.java
@@ -30,6 +30,34 @@ public class Action {
 	}
 	
 	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((coord == null) ? 0 : coord.hashCode());
+		result = prime * result + ((type == null) ? 0 : type.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Action other = (Action) obj;
+		if (coord == null) {
+			if (other.coord != null)
+				return false;
+		} else if (!coord.equals(other.coord))
+			return false;
+		if (type != other.type)
+			return false;
+		return true;
+	}
+
+	@Override
 	public String toString() {
 		return type + "@" + coord;
 	}
