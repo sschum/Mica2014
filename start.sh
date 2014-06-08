89190d18f389033929013d2e112f187bedd67324
sschum <s.schumann@tarent.de>
2014-06-08 16:37:18 +0200

    * Im Startskript die Möglichkeit zum Debuggen eingebaut

--- start.sh
+++ start.sh
@@ -20,6 +20,7 @@
 
 INPUT_FILE=$1
 JAR_NAME="RayShip.jar"
+JAVA_ARGS=
 
 SCRIPT_NAME=`basename $0`
 UNIX_STYLE_HOME=$(cd `dirname "${BASH_SOURCE[0]}"` && pwd)/`basename "${BASH_SOURCE[0]}"`
@@ -29,10 +30,21 @@ UNIX_STYLE_HOME=`echo $UNIX_STYLE_HOME | sed "s/$SCRIPT_NAME//g"`
 # MAIN
 ####
 
+while [ $# -gt 0 ]; do                   
+	case "$1" in
+	-debug)
+		JAVA_ARGS=$JAVA_ARGS" -Xdebug -Xrunjdwp:server=y,transport=dt_socket,address=8000,suspend=n"
+	;;
+	esac
+			
+	shift
+done
+
+
 if [ -z "$INPUT_FILE" ]; then
-    java -jar $UNIX_STYLE_HOME/$JAR_NAME
+    java $JAVA_ARGS -jar $UNIX_STYLE_HOME/$JAR_NAME
 else
 	echo "Skript wird ausgeführt: "$INPUT_FILE
 	
-	cat $INPUT_FILE | sed 's/#.*//g' | grep -v '^\s*$' | sed 's/^\s*//g' | java -jar $UNIX_STYLE_HOME/$JAR_NAME
+	cat $INPUT_FILE | sed 's/#.*//g' | grep -v '^\s*$' | sed 's/^\s*//g' | java $JAVA_ARGS -jar $UNIX_STYLE_HOME/$JAR_NAME
 fi
