92afca1e113ea6cb51dca9f880382857568212f3
sschum <s.schumann@tarent.de>
2014-06-08 15:34:34 +0200

    * ShellSkript für das Starten der Anwendung - man kann es benutzen um die Anwendung via Skripts zu steuern

---
+++ start.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#
+# Dieses Script startet die runnable-jar-file.
+# Zusätzlich kann man noch direkt eine Datei übergeben.
+# Der Inhalt der Datei wird dann als Kommandoeingabe für
+# Die Anwendung benutzt. 
+#
+# Diese "Skriptdateien" können Kommentare in folgender Form beinhalten:
+#
+# ...
+# #Dies ist ein Kommentar
+# befehlX #Dies ist ein Kommentar
+# befelY
+# ...
+#
+
+####
+# VARIABLES
+####
+
+INPUT_FILE=$1
+JAR_NAME="RayShip.jar"
+
+SCRIPT_NAME=`basename $0`
+UNIX_STYLE_HOME=$(cd `dirname "${BASH_SOURCE[0]}"` && pwd)/`basename "${BASH_SOURCE[0]}"`
+UNIX_STYLE_HOME=`echo $UNIX_STYLE_HOME | sed "s/$SCRIPT_NAME//g"`
+
+####
+# MAIN
+####
+
+if [ -z "$INPUT_FILE" ]; then
+    java -jar $UNIX_STYLE_HOME/$JAR_NAME
+else
+	echo "Skript wird ausgeführt: "$INPUT_FILE
+	
+	cat $INPUT_FILE | sed 's/#.*//g' | grep -v '^$' | java -jar $UNIX_STYLE_HOME/$JAR_NAME
+fi
