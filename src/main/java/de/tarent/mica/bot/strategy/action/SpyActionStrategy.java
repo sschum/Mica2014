555cf662948b0cb44232e8126aea3b82bae785be
sschum <s.schumann@tarent.de>
2014-06-09 11:04:10 +0200

    * Neue ActionStrategy: Spionage ist nun möglich :)

---
+++ src/main/java/de/tarent/mica/bot/strategy/action/SpyActionStrategy.java
@@ -0,0 +1,103 @@
+package de.tarent.mica.bot.strategy.action;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+import de.tarent.mica.Action;
+import de.tarent.mica.Action.Type;
+import de.tarent.mica.bot.strategy.StrategyStats;
+import de.tarent.mica.model.Coord;
+import de.tarent.mica.model.World;
+import de.tarent.mica.model.element.Destroyer;
+import de.tarent.mica.model.element.Ship;
+import de.tarent.mica.model.element.Ship.Orientation;
+
+@StrategyStats(description = "Diese Strategie spioniert (wenn m\u00f6glich) den Gegner aus.")
+public class SpyActionStrategy extends ActionStrategy {
+	protected final List<Coord> spyPoints;
+	
+	public SpyActionStrategy(Coord...spyPoints) {
+		this.spyPoints = new ArrayList<Coord>(Arrays.asList(spyPoints));
+	}
+
+	@Override
+	public Action getActionDecision(World world) {
+		if(canSpy(world)){
+			return nextSpyAction(world);
+		}
+		
+		return null;
+	}
+
+	protected boolean canSpy(World world) {
+		//ich kann nur spionieren, wenn Destroyer 
+		//auf einer ebene eine gemeinsame Position besitzen
+		List<Destroyer> destroyers = new ArrayList<Destroyer>(2);
+		
+		for(Ship s : world.getOwnShips()){
+			if(s instanceof Destroyer) destroyers.add((Destroyer)s);
+		}
+		
+		int theoreticallyPossible = getPossibleSpyCount(destroyers);
+		if(theoreticallyPossible > 0){
+			int usedSpy = world.getSpecialAttackCount(Destroyer.class);
+			int possible = theoreticallyPossible - usedSpy;
+
+			return possible > 0;
+		}
+		
+		return false;
+	}
+
+	private int getPossibleSpyCount(List<Destroyer> destroyers) {
+		int count = 0;
+		
+		for(Coord d1Coord : destroyers.get(0).getSpace()){
+			for(Coord d2Coord : destroyers.get(1).getSpace()){
+				if(isCoordSameLevel(d1Coord, d2Coord)) count++;
+			}
+		}
+		
+		
+		if(	((destroyers.get(0).getOrientation() == Orientation.SUED || destroyers.get(0).getOrientation() == Orientation.NORD) &&
+			(destroyers.get(1).getOrientation() == Orientation.SUED || destroyers.get(1).getOrientation() == Orientation.NORD)) 
+			
+			||
+			
+			((destroyers.get(0).getOrientation() == Orientation.OST || destroyers.get(0).getOrientation() == Orientation.WEST) &&
+			(destroyers.get(1).getOrientation() == Orientation.OST || destroyers.get(1).getOrientation() == Orientation.WEST))	){
+			
+			//beide sind horizontal/vertikal ausgerichtet
+			return count;
+		}
+
+		//einer ist vertikal und der andere horiozontal ausgerichtet
+		//damit können (theoretisch) nur höchstens ein Punkt infrage kommen ;)
+		if(count > 0) return 1;
+		return 0;
+	}
+
+	private boolean isCoordSameLevel(Coord c1, Coord c2) {
+		//horizontal gleich
+		if(c1.getY() == c2.getY()) return true;
+		
+		//vertikal gleich
+		if(c1.getX() == c2.getX()) return true;
+		
+		return false;
+	}
+
+	protected Action nextSpyAction(World world) {
+		Iterator<Coord> iter = spyPoints.iterator();
+		
+		while(iter.hasNext()){
+			Coord next = iter.next();
+			iter.remove();
+			return new Action(Type.SPY_DRONE, next);
+		}
+		
+		return null;
+	}
+}
---
+++ src/test/java/de/tarent/mica/bot/strategy/action/SpyActionStrategyTest.java
@@ -0,0 +1,136 @@
+package de.tarent.mica.bot.strategy.action;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+import org.junit.Test;
+
+import de.tarent.mica.Action;
+import de.tarent.mica.Action.Type;
+import de.tarent.mica.model.Coord;
+import de.tarent.mica.model.World;
+import de.tarent.mica.model.element.Destroyer;
+import de.tarent.mica.model.element.Ship.Orientation;
+
+public class SpyActionStrategyTest {
+	SpyActionStrategy toTest = new SpyActionStrategy();
+	
+	@Test
+	public void canSpy(){
+		World world = new World(10, 10);
+		
+		/*
+		 *   0 1 2 3 4
+		 * A
+		 * B   * * *
+		 * C
+		 * D * * *
+		 * E
+		 */
+		world.placeOwnShip(new Destroyer(Orientation.OST, new Coord("B1")));
+		world.placeOwnShip(new Destroyer(Orientation.OST, new Coord("D0")));
+		assertTrue(toTest.canSpy(world));
+		
+		/*
+		 *   0 1 2 3 4
+		 * A   * * *
+		 * B   
+		 * C     *
+		 * D     *
+		 * E     *
+		 */
+		world = new World(10, 10);
+		world.placeOwnShip(new Destroyer(Orientation.OST, new Coord("A1")));
+		world.placeOwnShip(new Destroyer(Orientation.SUED, new Coord("C2")));
+		assertTrue(toTest.canSpy(world));
+		
+		/*
+		 *   0 1 2 3 4
+		 * A
+		 * B   * * *
+		 * C 
+		 * D * 
+		 * E *
+		 * F *
+		 */
+		world = new World(10, 10);
+		world.placeOwnShip(new Destroyer(Orientation.OST, new Coord("B1")));
+		world.placeOwnShip(new Destroyer(Orientation.SUED, new Coord("D0")));
+		assertFalse(toTest.canSpy(world));
+		
+		/*
+		 *   0 1 2 3 4
+		 * A
+		 * B   * * *
+		 * C 
+		 * D     * 
+		 * E     *
+		 * F     *
+		 */
+		world = new World(10, 10);
+		world.registerSpecialAttack(Destroyer.class); //es wurde schon spoiniert
+		world.placeOwnShip(new Destroyer(Orientation.OST, new Coord("B1")));
+		world.placeOwnShip(new Destroyer(Orientation.SUED, new Coord("D2")));
+		assertFalse(toTest.canSpy(world));
+		
+		/*
+		 *   0 1 2 3 4
+		 * A
+		 * B   * * *
+		 * C
+		 * D * * *
+		 * E
+		 */
+		world = new World(10, 10);
+		world.registerSpecialAttack(Destroyer.class); //es wurde schon spoiniert
+		world.placeOwnShip(new Destroyer(Orientation.OST, new Coord("B1")));
+		world.placeOwnShip(new Destroyer(Orientation.OST, new Coord("D0")));
+		assertTrue(toTest.canSpy(world));
+		
+		/*
+		 *   0 1 2 3 4
+		 * A
+		 * B   * * *
+		 * C
+		 * D * * *
+		 * E
+		 */
+		world = new World(10, 10);
+		world.registerSpecialAttack(Destroyer.class);
+		world.registerSpecialAttack(Destroyer.class); //es wurde schon zweimal spoiniert
+		world.placeOwnShip(new Destroyer(Orientation.OST, new Coord("B1")));
+		world.placeOwnShip(new Destroyer(Orientation.OST, new Coord("D0")));
+		assertFalse(toTest.canSpy(world));
+	}
+	
+	@Test
+	public void nextSpyAction(){
+		toTest = new SpyActionStrategy();	//keine spionagepunkte definiert
+		
+		assertNull(toTest.nextSpyAction(null));
+		
+		toTest = new SpyActionStrategy(new Coord("A1"), new Coord("A2"));
+		Action result = toTest.nextSpyAction(null);
+		
+		assertEquals(Type.SPY_DRONE, result.getType());
+		assertEquals(new Coord("A1"), result.getCoord());
+		
+		result = toTest.nextSpyAction(null);
+		
+		assertEquals(Type.SPY_DRONE, result.getType());
+		assertEquals(new Coord("A2"), result.getCoord());
+	}
+	
+	@Test
+	public void getActionDecision(){
+		SpyActionStrategy spy = spy(toTest);
+		doReturn(false).when(spy).canSpy(any(World.class));
+		
+		assertNull(spy.getActionDecision(null));
+		
+		doReturn(true).when(spy).canSpy(any(World.class));
+		doReturn(new Action(Type.SPY_DRONE, new Coord("A1"))).when(spy).nextSpyAction(any(World.class));
+		
+		assertEquals(spy.nextSpyAction(null), spy.getActionDecision(null));
+	}
+}
