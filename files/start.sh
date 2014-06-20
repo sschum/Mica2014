bb54e7b4110141cfff78834451a61fee0eeb32bb
Sven Schumann <s.schumann@tarent.de>
2014-06-20 14:10:34 +0200

    fix start-script

--- files/start.sh
+++ files/start.sh
@@ -1,2 +1,2 @@
-java -jar target/uber-battleships-server-0.2.jar server config.yaml
+java -jar uber-battleships-server-0.3.jar server config.yaml
 #java -Doutput.wait=200 -Dreplay.file=replay.log -jar target/uber-battleships-server-0.2.jar server config.yaml
