4d88ff0a1cbac6aa0d8f697f6736b65de4462bfc
Robert Linden <r.linden@tarent.de>
2014-06-21 23:55:19 +0200

    updated server-version in start.sh

--- files/start.sh
+++ files/start.sh
@@ -1,2 +1,2 @@
-java -Dmanager.password=foo -jar target/uber-battleships-server-0.2.jar server config.yaml
-#java -Dmanager.password=foo -Doutput.wait=200 -Dreplay.file=replay.log -jar target/uber-battleships-server-0.2.jar server config.yaml
+java -Dmanager.password=foo -jar target/uber-battleships-server-0.4.jar server config.yaml
+#java -Dmanager.password=foo -Doutput.wait=200 -Dreplay.file=replay.log -jar target/uber-battleships-server-0.4.jar server config.yaml
