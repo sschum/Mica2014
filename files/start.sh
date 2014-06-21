a9e16246eb4f3b6a243a8d13fbd4495212b1e2da
Robert Linden <r.linden@tarent.de>
2014-06-21 23:53:52 +0200

    Server v4. Multi-round-games. Manager-user.

--- files/start.sh
+++ files/start.sh
@@ -1,2 +1,2 @@
-java -jar target/uber-battleships-server-0.2.jar server config.yaml
-#java -Doutput.wait=200 -Dreplay.file=replay.log -jar target/uber-battleships-server-0.2.jar server config.yaml
+java -Dmanager.password=foo -jar target/uber-battleships-server-0.2.jar server config.yaml
+#java -Dmanager.password=foo -Doutput.wait=200 -Dreplay.file=replay.log -jar target/uber-battleships-server-0.2.jar server config.yaml
