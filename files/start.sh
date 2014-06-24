a90f21d218460db957302b0314b8f2418f02407a
Robert Linden <r.linden@tarent.de>
2014-06-24 13:29:24 +0200

    v5: multi-round-bugfix

--- files/start.sh
+++ files/start.sh
@@ -1,2 +1,2 @@
-java -Dmanager.password=foo -jar target/uber-battleships-server-0.4.jar server config.yaml
+java -Dmanager.password=foo -jar uber-battleships-server-0.5.jar server config.yaml
 #java -Dmanager.password=foo -Doutput.wait=200 -Dreplay.file=replay.log -jar target/uber-battleships-server-0.4.jar server config.yaml
