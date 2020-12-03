d199fc6bb3655f6f024667b1546fddd128c11014
sschum <s.schumann@tarent.de>
2020-12-03 14:59:33 +0100

    new server version

--- files/start.sh
+++ files/start.sh
@@ -1,2 +1,2 @@
-java -Dmanager.password=foo -jar uber-battleships-server-0.6.jar server config.yaml
-#java -Dmanager.password=foo -Doutput.wait=200 -Dreplay.file=replay.log -jar uber-battleships-server-0.6.jar server config.yaml
+java -Dmanager.password=foo -jar uber-battleships-server-0.8.jar server config.yaml
+#java -Dmanager.password=foo -Doutput.wait=200 -Dreplay.file=replay.log -jar uber-battleships-server-0.8.jar server config.yaml
